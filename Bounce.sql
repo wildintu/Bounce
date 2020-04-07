6a78d63532b88860c05eea5d558be826d4bfa115
wildintu <hello@wildintu.com>
2020-04-07 13:26:28 -0500

    most recent bounce sql

--- Bounce.sql
+++ Bounce.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.29, for macos10.14 (x86_64)
 --
--- Host: 127.0.0.1    Database: bounce-schema
+-- Host: 127.0.0.1    Database: bounce
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	5.7.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,44 +16,15 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `main`
---
-
-DROP TABLE IF EXISTS `main`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `main` (
-  `id` int NOT NULL,
-  `userid` int NOT NULL,
-  `pollName` char(100) NOT NULL,
-  `pollInput` text NOT NULL,
-  `ideaType` varchar(45) NOT NULL,
-  `timeStamp` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `userid_UNIQUE` (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `main`
---
-
-LOCK TABLES `main` WRITE;
-/*!40000 ALTER TABLE `main` DISABLE KEYS */;
-/*!40000 ALTER TABLE `main` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `Secondary`
 --
 
 DROP TABLE IF EXISTS `Secondary`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Secondary` (
-  `id` int NOT NULL,
-  `origId` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `origId` int(11) NOT NULL,
   `secName` char(100) DEFAULT NULL,
   `secInput` text NOT NULL,
   `anonymous` tinyint(1) NOT NULL,
@@ -62,7 +33,7 @@ CREATE TABLE `Secondary` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `id_idx` (`origId`),
   CONSTRAINT `mainPrimary` FOREIGN KEY (`origId`) REFERENCES `main` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,6 +43,35 @@ CREATE TABLE `Secondary` (
 LOCK TABLES `Secondary` WRITE;
 /*!40000 ALTER TABLE `Secondary` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Secondary` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `main`
+--
+
+DROP TABLE IF EXISTS `main`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `main` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `pollName` char(100) NOT NULL,
+  `pollInput` text NOT NULL,
+  `ideaType` varchar(45) NOT NULL,
+  `timeStamp` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `userid_UNIQUE` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `main`
+--
+
+LOCK TABLES `main` WRITE;
+/*!40000 ALTER TABLE `main` DISABLE KEYS */;
+/*!40000 ALTER TABLE `main` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -83,4 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-07 10:59:15
+-- Dump completed on 2020-04-07 13:25:41
