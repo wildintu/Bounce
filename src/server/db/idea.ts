e73fa31a8697a4d06e32a9f9c577d01be736bc3f
wildintu <hello@wildintu.com>
2020-04-07 10:39:38 -0500

    made ideats template

--- src/server/db/idea.ts
+++ src/server/db/idea.ts
@@ -1,23 +1,23 @@
 import { Query } from './index';
 
-// const getAllSessions = async () => 
+// const getAllIdeas = async () => 
 // Query('SELECT * from tags');
 
-// const getOneSession = async (id: number) => {
+// const getOneIdea = async (id: number) => {
 //     return Query('SELECT * FROM tags WHERE id = ?', [id]);
 // };
 
-// const postOneSession = async (name: string, description: string, displaytype: any) => {
+// const postOneIdea = async (name: string, description: string, displaytype: any) => {
 //     let values = [name, description, displaytype];
 //     return Query('INSERT INTO sessions(name,description,displaytype) VALUES(?,?,?)', values)
 // }
 
-// const updateOneSession = async (id: number, name: string, description: string, displaytype: any) => {
+// const updateOneIdea = async (id: number, name: string, description: string, displaytype: any) => {
 //     let values = [name, description, displaytype, id];
 //     return Query('UPDATE sessions SET title= ?,content=? ,authorid=? WHERE id = ?', values);
 // }
 
-// const deleteSession = async (id: number) =>
+// const deleteIdea = async (id: number) =>
 // Query('DELETE FROM sessions WHERE id =?', [id])
 
 // export default {
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,89 @@
 import * as express from 'express';
+import DB from './db';
 
 const router = express.Router();
 
 router.get('/api/hello', (req, res, next) => {
     res.json('World');
 });
+
+// router.get('/api/blogs', async (req, res) => {
+//     try {
+//         let blogs = await DB.Blogs.all();
+//         res.json(blogs);
+//     } catch(e) {
+//         console.log(e);
+//         res.sendStatus(500);
+//     }
+// })
+
+// router.get('/api/blogs/:id', async (req, res) => {
+//     try {
+//         let blogs = await DB.Blogs.one(parseInt(req.params.id, 10));
+//         res.json(blogs[0]);
+//     } catch(e) {
+//         console.log(e);
+//         res.sendStatus(500);
+//     }
+// })
+
+// router.post('/api/blogs', async (req, res) => {
+//     let title = req.body.title;
+//     let content = req.body.content;
+//     let authorid = parseInt(req.body.authorid, 10);
+//     let tagid = parseInt(req.body.tagid, 10)
+//     try {
+//         let blogs = await DB.Blogs.post(title, content, authorid, tagid);
+//         res.json(blogs);
+//     } catch(e) {
+//         console.log(e);
+//         res.sendStatus(500);
+//     }
+// })
+
+// router.put('/api/blogs/:id?', async (req, res) => {
+//     let id = parseInt(req.params.id, 10);
+//     let title = req.body.title;
+//     let content = req.body.content;
+//     let authorid = parseInt(req.body.authorid, 10);
+//     try {
+//         let blogs = await DB.Blogs.put(id, title, content, authorid);
+//         res.json(blogs);
+//     } catch(e) {
+//         console.log(e);
+//         res.sendStatus(500);
+//     }
+// })
+
+// router.delete('/api/blogs/:id?', async (req, res) => {
+//     let id = parseInt(req.params.id, 10);
+//     try {
+//         let blogs = await DB.Blogs.del(id);
+//         res.json(blogs);
+//     } catch(e) {
+//         console.log(e);
+//         res.sendStatus(500);
+//     }
+// })
+
+// router.get('/api/tags', async (req, res) => {
+//     try {
+//         let tags = await DB.Tags.tAll();
+//         res.json(tags);
+//     } catch(e) {
+//         console.log(e);
+//         res.sendStatus(500);
+//     }
+// })
+
+// router.get('/api/tags/:id', async (req, res) => {
+//     try {
+//         let tags = await DB.Tags.tOne(parseInt(req.params.id, 10));
+//         res.json(tags);
+//     } catch(e) {
+//         console.log(e);
+//         res.sendStatus(500);
+//     }
+// })
 
 export default router;
