07bb8a6fdfc9cbb97da1a0e6a1279b90c993b34f
wildintu <hello@wildintu.com>
2020-04-07 09:27:42 -0500

    added diagramjs module and updated sessionts

--- src/server/db/session.ts
+++ src/server/db/session.ts
@@ -7,6 +7,19 @@ import { Query } from './index';
 //     return Query('SELECT * FROM tags WHERE id = ?', [id]);
 // };
 
+const postOneSession = async (name: string, description: string, displaytype: any) => {
+    let values = [name, description, displaytype];
+    return Query('INSERT INTO sessions(name,description,displaytype) VALUES(?,?,?)', values)
+}
+
+const updateOneSession = async (id: number, name: string, description: string, displaytype: any) => {
+    let values = [name, description, displaytype, id];
+    return Query('UPDATE sessions SET title= ?,content=? ,authorid=? WHERE id = ?', values);
+}
+
+const deleteSession = async (id: number) =>
+Query('DELETE FROM sessions WHERE id =?', [id])
+
 // export default {
 //     getAllSessions,
 //     getOneSession
