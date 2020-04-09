edc950ba0aa481db17ebff1afb6dbe6a60f73b0a
allsgood1 <ausgood1@gmail.com>
2020-04-08 19:41:31 -0500

    merge remote repository w local

--- src/client/Components/Collaboration.tsx
+++ src/client/Components/Collaboration.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from 'react';
 
 const Collaboration: React.FC<CollabProps> = () => {
@@ -22,29 +21,4 @@ const Collaboration: React.FC<CollabProps> = () => {
 
 interface CollabProps { };
 
-=======
-import * as React from 'react';
-
-const Collaboration: React.FC<CollabProps> = () => {
-
-    return (
-        <main className="container my-5">
-            <section className="justify-content-center">
-                <div className="card shadow">
-                    <div className="card-body text-center">
-                        <h1 className="text-primary text-center">Group Project Ideas</h1>
-                        <p className="text-primary">gonna figure out how to make this inset webb of ideas</p>
-                       
-                    </div>
-                </div>
-            </section>
-        </main>
-    );
-
-
-};
-
-interface CollabProps { };
-
->>>>>>> 3875946732e8a8a7a536384da28065519d337974
 export default Collaboration;
