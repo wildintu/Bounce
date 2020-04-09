fdc7b13ddda4830bdcde1dab8b7335d927245dbd
jenniferflo24 <59664282+jenniferflo24@users.noreply.github.com>
2020-04-08 22:21:29 -0500

    added morgan node module and inserted into server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,16 @@
 import * as express from 'express';
-import apiRouter from './routes';
 import * as path from 'path';
+import * as morgan from 'morgan';
+
+import apiRouter from './routes';
 
 const app = express();
 app.use(express.json());
 
 
 app.use(express.static('public'));
+
+app.use(morgan('dev'));
 app.use(apiRouter);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'))
