a23136219f83822142e0808dc6602b9f19f38583
wildintu <hello@wildintu.com>
2020-04-10 17:17:55 -0500

    updated SQL file to include the user and grants

--- Bounce.sql
+++ Bounce.sql
@@ -14,7 +14,13 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+-- CREATE USER 'bounce'@'localhost';
+-- IDENTIFIED BY 'password123';
+-- GRANT ALL PRIVILEGES
+-- ON bounce.*
+-- TO 'bounce'@'localhost';
+create database bounce;
+use bounce;
 --
 -- Table structure for table `Secondary`
 --
